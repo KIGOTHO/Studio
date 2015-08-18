4ef001397be7f4ffb10fa028ca206c22f6584373
michael <mkigotho2@gmail.com>
2015-08-19 00:41:15 +0300

    opening a new project to learn android studio

---
+++ app/src/main/java/com/example/mike/hellostudio/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.mike.hellostudio;
+
+import android.support.v7.app.ActionBarActivity;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+   
+
+        return super.onOptionsItemSelected(item);
+    }
+}
