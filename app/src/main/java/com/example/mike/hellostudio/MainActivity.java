c048721f943e2e00b89b672e26eabd80ca192657
michael <mkigotho2@gmail.com>
2015-08-19 00:44:06 +0300

    importing files that are requested by methods

--- app/src/main/java/com/example/mike/hellostudio/MainActivity.java
+++ app/src/main/java/com/example/mike/hellostudio/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.mike.hellostudio;
 
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
